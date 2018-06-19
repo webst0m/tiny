fd22c736df9322ac7d14a3650d55c9910cc3d9c2
taoyu <1255803549@qq.com>
2018-06-19 23:05:41 +0800

    fix bugs

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -10,7 +10,19 @@ class CategoryService
     // todo cache
     public function getAllByType($type)
     {
+        $user = auth()->user();
+        $isSuperAdmin = $user->hasRole('super_admin');
+        if (!$isSuperAdmin) {
+            $canAccessCategoryIDs = $user->categories->pluck('id');
+        }else{
+            $canAccessCategoryIDs = collect([]);
+        }
         $topicCategories = Category::topCategories()->ordered()->oldest()->get();
+        foreach($topicCategories as $k=>$topicCategory) {
+            if(!$isSuperAdmin && !$canAccessCategoryIDs->contains($topicCategory->id)){
+                $topicCategories->splice($k ,1);
+            }
+        }
         $topicCategories->load(['children' => function ($query) use ($type) {
             $query->byType($type)->ordered()->oldest();
         }]);
@@ -19,30 +31,28 @@ class CategoryService
                 return $category->type == $type || $category->children->isNotEmpty();
             });
         }
+        
+        foreach($topicCategories as $topicCategory) {
+            foreach ($topicCategory->children as $k=>$child){
+                if(!$isSuperAdmin && !$canAccessCategoryIDs->contains($child->id)){
+                    $topicCategory->children->splice($k ,1);
+                } 
+            }
+        }
+
         return $topicCategories;
     }
 
     public function visualOutput($type = null, $indentStr = '-')
     {
-        $user = auth()->user();
-        $isSuperAdmin = $user->hasRole('super_admin');
-        if (!$isSuperAdmin) {
-            $canAccessCategoryIDs = $user->categories->pluck('id');
-        }else{
-            $canAccessCategoryIDs = collect([]);
-        }
-
         $topicCategories = $this->getAllByType($type);
         $collect = collect([]);
         foreach ($topicCategories as $topicCategory) {
-            if ($isSuperAdmin || $canAccessCategoryIDs->contains($topicCategory->id)) {
-                $collect->push(['indent_str' => '', 'data' => $topicCategory]);
-                foreach ($topicCategory->children as $child) {
-                    if ($isSuperAdmin || $canAccessCategoryIDs->contains($child->id)) {
-                        $collect->push(['indent_str' => $indentStr, 'data' => $child]);
-                    }
-                }
+            $collect->push(['indent_str' => '', 'data' => $topicCategory]);
+            foreach ($topicCategory->children as $child) {
+                    $collect->push(['indent_str' => $indentStr, 'data' => $child]);
             }
+            
         }
         return $collect;
     }
